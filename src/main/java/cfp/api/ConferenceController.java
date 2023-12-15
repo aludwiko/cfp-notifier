d80eff57aeadf4a463520767639f7c6d9b924bac
Andrzej Ludwikowski <andrzej.ludwikowski@lightbend.com>
2023-12-15 09:13:24 +0100

    small updates

--- src/main/java/cfp/api/ConferenceController.java
+++ src/main/java/cfp/api/ConferenceController.java
@@ -1,11 +1,14 @@
-package cfp.action;
+package cfp.api;
 
 import cfp.entity.ConferenceEntity;
 import cfp.entity.CreateConference;
 import kalix.javasdk.action.Action;
-
 import kalix.javasdk.client.ComponentClient;
-import org.springframework.web.bind.annotation.*;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import java.time.LocalDate;
 import java.util.UUID;
@@ -13,6 +16,9 @@ import java.util.concurrent.CompletionStage;
 
 @RequestMapping("/api/slack/conferences")
 public class ConferenceController extends Action {
+
+    private final Logger logger = LoggerFactory.getLogger(this.getClass());
+
 
     private final ComponentClient componentClient;
 
@@ -37,7 +43,8 @@ public class ConferenceController extends Action {
             @RequestParam(required = false) String trigger_id,
             @RequestParam(required = false) String api_app_id
     ) {
-        System.out.println("token: " + token + ", team_id: " + team_id + ", team_domain: " + team_domain + ", enterprise_id: " + enterprise_id + ", enterprise_name: " + enterprise_name + ", channel_id: " + channel_id + ", channel_name: " + channel_name + ", user_id: " + user_id + ", user_name: " + user_name + ", command: " + command + ", text: " + text + ", response_url: " + response_url + ", trigger_id: " + trigger_id + ", api_app_id: " + api_app_id);
+
+        logger.info("Adding conference, token: " + token + ", team_id: " + team_id + ", team_domain: " + team_domain + ", enterprise_id: " + enterprise_id + ", enterprise_name: " + enterprise_name + ", channel_id: " + channel_id + ", channel_name: " + channel_name + ", user_id: " + user_id + ", user_name: " + user_name + ", command: " + command + ", text: " + text + ", response_url: " + response_url + ", trigger_id: " + trigger_id + ", api_app_id: " + api_app_id);
 
         var conferenceId = UUID.randomUUID().toString();
         CreateConference createConference = parseCreateConference(text);
@@ -50,10 +57,12 @@ public class ConferenceController extends Action {
 
         // transform response
         CompletionStage<Effect<String>> effect =
-                call.handle((empty, error) -> {
+                call.handle((conference, error) -> {
                     if (error == null) {
-                        return effects().reply(conferenceId);
+                        logger.info("Created conference: " +  conference);
+                        return effects().reply(conference.id());
                     } else {
+                        logger.error("Failed to create conference", error);
                         return effects().error("Failed to create conference, please retry");
                     }
                 });
